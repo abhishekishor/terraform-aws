269113f5eba965a114229e082fba8d6bf6ffcda8
abhishekishor <kishoravishek@gmail.com>
2023-06-17 11:11:36 +0000

    Configure Levels

--- level2/data.tf
+++ level2/data.tf
@@ -3,10 +3,10 @@ data "terraform_remote_state" "level1" {
   backend = "s3"
   config = {
 
-    bucket         = "terra-remote-st"
-    key            = "level1.tfstate"
-    region         = "ap-south-1"
-      
+    bucket = "terra-remote-st"
+    key    = "level1.tfstate"
+    region = "ap-south-1"
+
   }
 
 }
