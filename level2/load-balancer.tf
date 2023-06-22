2c06af43a521d9c5f3cbb7dc69bdaf682bccb04c
abhishekishor <kishoravishek@gmail.com>
2023-06-22 10:12:30 +0000

    2nd try

--- level2/load-balancer.tf
+++ level2/load-balancer.tf
@@ -1,77 +1,9 @@
-resource "aws_security_group" "load_balancer" {
-  name        = "${var.env_code}-load_balancer"
-  description = "Allow private traffics"
-  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
+module "lb" {
 
-  ingress {
-    description = "HTTP from Everywhere"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ source = "../modules/load-balancer"
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    name = "${var.env_code}-load_balancer"
-  }
-
-}
-
-resource "aws_lb" "main" {
-
-  name               = var.env_code
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.load_balancer.id]
-  subnets            = data.terraform_remote_state.level1.outputs.public_subnet_id
-
-  tags = {
-
-    Name = var.env_code
-
-  }
-
-}
-
-resource "aws_lb_target_group" "main" {
-
-  name     = var.env_code
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.terraform_remote_state.level1.outputs.vpc_id
-
-  health_check {
-
-    enabled             = true
-    path                = "/"
-    port                = "traffic-port"
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-    timeout             = 5
-    interval            = 10
-    matcher             = 200
-
-  }
-
-}
-
-resource "aws_lb_listener" "main" {
-
-  load_balancer_arn = aws_lb.main.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-
-  }
+ env_code = var.env_code
+ vpc_id = data.terraform_remote_state.level1.outputs.vpc_id
+ subnet_id = data.terraform_remote_state.level1.outputs.public_subnet_id
 
 }
